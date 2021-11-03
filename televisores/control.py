b3496ba9210d018056824e497ef02901aa5e84c5
Juanes8a <80339256+Juanes8a@users.noreply.github.com>
2021-11-02 20:04:47 -0500

    porfa

--- televisores/control.py
+++ televisores/control.py
@@ -7,22 +7,22 @@ class Control:
         self._tv=tele    
         tele.setControl(self)
     def turnOn(self):
-        return TV.turnOn(self)      
+        return TV.turnOn(self._tv)      
     def turnOff(self):
-        return TV.turnOff(self)   
+        return TV.turnOff(self._tv)   
 
     def setCanal(self,canal):
         self._canal=canal
 
     def canalUp(self):
-        return TV.canalUp(self)     
+        return TV.canalUp(self._tv)     
     def canalDown(self):
-        return TV.canalDown(self)
+        return TV.canalDown(self._tv)
 
     def volumenUp(self):
-        return TV.volumenUp(self)     
+        return TV.volumenUp(self._tv)     
     def volumenDown(self):
-        return TV.volumenDown(self) 
+        return TV.volumenDown(self._tv) 
 
     def getTv(self):
         return self._tv
